e4126a6c71f05e5452e6dbb61b20e5e9e935b0f7
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-07-30 19:26:32 +0530

    Created Home - Section4, Section5 & Section 6

---
+++ src/pages/home/Section5.tsx
@@ -0,0 +1,81 @@
+// components
+import Title from "../../common/Title";
+
+const PLATFORMS: string[] = [
+  "anagram",
+  "galileo",
+  "dialectic",
+  "lemniscap",
+  "robot-ventures",
+  "axelar",
+  "arbitrum",
+  "gsr",
+  "chorus-one",
+  "reverie",
+];
+const INVESTORS: string[] = [
+  "Zaki Manian",
+  "Waikit Lau",
+  "Leland Lee",
+  "Calvin Liu",
+  "George Lambeth",
+  "Xavier Meegan",
+  "Stefan Coolican",
+  "Richard Malone",
+  "Collin Myers",
+  "Batu",
+];
+
+const Section5 = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center gap-8 p-4 sm:px-8">
+        {/* LEFT BORDER IMAGE */}
+        <img
+          loading="lazy"
+          className="hidden xl:block w-36 h-96"
+          src="/home/backers/border.svg"
+          alt="Decoration"
+        />
+
+        {/* BACKERS */}
+        <div className="flex flex-col gap-12 justify-center items-center">
+          <Title title="Backers" />
+          {/* PLATFORM IMAGES */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-8 lg:gap-12 items-center justify-between">
+            {PLATFORMS.map((item, index) => (
+              <img
+                loading="lazy"
+                key={index}
+                className="max-w-36 w-full"
+                src={`/home/backers/${item}.svg`}
+                alt={item}
+              />
+            ))}
+          </div>
+
+          {/* INVESTOR NAMES */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 text-center items-center justify-center">
+            {INVESTORS.map((item, index) => (
+              <span
+                className="px-4 py-1 text-xl"
+                key={index}
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        {/* RIGHT BORDER IMAGE */}
+        <img
+          loading="lazy"
+          className="hidden xl:block w-36 h-96 rotate-180"
+          src="/home/backers/border.svg"
+          alt="Decoration"
+        />
+      </div>
+    </div>
+  );
+};
+export default Section5;
---
+++ src/pages/home/Section6.tsx
@@ -0,0 +1,107 @@
+// components
+import Title from "../../common/Title";
+import Slider from "../../common/Slider";
+
+const Section6 = () => {
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="mb-8">
+        <Title title="Ecosystem" />
+      </div>
+      <div className="lg:mx-auto lg:w-4/5">
+        <Slider
+          gap="2rem"
+          width={80}
+          height={80}
+          speed={60}
+          play={false}
+          direction="left"
+          items={[
+            <a href="https://www.altlayer.io/">
+              <img className="w-16" src="/home/ecosystem/alt-layer.svg" />
+            </a>,
+            <a href="https://anoma.net/">
+              <img className="w-16" src="/home/ecosystem/anoma.svg" />
+            </a>,
+            <a href="http://arbitrum.io/">
+              <img className="w-16" src="/home/ecosystem/arbitrum.svg" />
+            </a>,
+            <a href="https://www.base.org/">
+              <img className="w-16" src="/home/ecosystem/base.svg" />
+            </a>,
+            <a href="https://celestia.org/">
+              <img className="w-16" src="/home/ecosystem/celestia.svg" />
+            </a>,
+            <a href="https://cosmos.network/">
+              <img className="w-16" src="/home/ecosystem/cosmos-hub.svg" />
+            </a>,
+            <a href="https://swap.cow.fi/">
+              <img className="w-16" src="/home/ecosystem/cow-swap.svg" />
+            </a>,
+            <a href="https://ethereum.org/en/">
+              <img className="w-12" src="/home/ecosystem/ethereum.svg" />
+            </a>,
+            <a href="https://www.fluent.xyz/">
+              <img className="w-16" src="/home/ecosystem/fluent.svg" />
+            </a>,
+            <a href="https://hyperfoundation.org/">
+              <img className="w-16" src="/home/ecosystem/hyper-liquid.svg" />
+            </a>,
+            <a href="https://mitosis.org/">
+              <img className="w-16" src="/home/ecosystem/mitosis.svg" />
+            </a>,
+            <a href="https://www.monad.xyz/">
+              <img className="w-16" src="/home/ecosystem/monad.svg" />
+            </a>,
+          ]}
+        />
+      </div>
+      <div className="lg:mx-auto lg:w-4/5">
+        <Slider
+          gap="2rem"
+          width={80}
+          height={80}
+          speed={60}
+          play={false}
+          direction="right"
+          items={[
+            <a href="https://www.noble.xyz/">
+              <img className="w-16" src="/home/ecosystem/noble.svg" />
+            </a>,
+            <a href="https://www.optimism.io/">
+              <img className="w-16" src="/home/ecosystem/opstack.svg" />
+            </a>,
+            <a href="https://phala.network/">
+              <img className="w-16" src="/home/ecosystem/phala.svg" />
+            </a>,
+            <a href="https://polygon.technology/">
+              <img className="w-16" src="/home/ecosystem/polygon.svg" />
+            </a>,
+            <a href="https://polymerlabs.org/">
+              <img className="w-16" src="/home/ecosystem/polymer.svg" />
+            </a>,
+            <a href="https://ritual.net/">
+              <img className="w-16" src="/home/ecosystem/ritual.svg" />
+            </a>,
+            <a href="https://solana.com/">
+              <img className="w-16" src="/home/ecosystem/solana.svg" />
+            </a>,
+            <a href="https://www.squidrouter.com/">
+              <img className="w-16" src="/home/ecosystem/squid.svg" />
+            </a>,
+            <a href="https://symbiotic.fi/">
+              <img className="w-16" src="/home/ecosystem/symbiotic.svg" />
+            </a>,
+            <a href="https://mountainprotocol.com/">
+              <img className="w-16" src="/home/ecosystem/usdm.svg" />
+            </a>,
+            <a href="https://xai.games/">
+              <img className="w-16" src="/home/ecosystem/xai.svg" />
+            </a>,
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
+export default Section6;
