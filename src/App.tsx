08ea8bfcbedc06a463aede7b849bf552dde262a1
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-08-14 20:27:22 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages & components
+import Header from "./common/Header/Header";
+import Home from "./pages/home/Home";
+import HowItWorks from "./pages/how-it-works/HowItWorks";
+import Ecosystem from "./pages/ecosystem/Ecosystem";
+import CApps from "./pages/c-apps/CApps";
+import Footer from "./common/Footer/Footer";
+import Articles from "./pages/articles/Articles";
+
+// css
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='text-xl text-center'>
-      Fairblock Website v2.0
+    <div className="font-marlide">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="how-it-works" element={<HowItWorks />} />
+          <Route path="ecosystem" element={<Ecosystem />} />
+          <Route path="c-apps" element={<CApps />} />
+          <Route path="articles" element={<Articles />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
