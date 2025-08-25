70025fce4800fc3e479aced72ee96d9e447902a5
pememoni <34263018+pememoni@users.noreply.github.com>
2025-08-25 08:54:42 -0400

    question-mark

--- src/pages/how-it-works/Section1.tsx
+++ src/pages/how-it-works/Section1.tsx
@@ -16,7 +16,7 @@ const Section1 = () => {
       {/* how-it-works content */}
       <div className="flex flex-col gap-4">
         <div className="text-left">
-          <Title title="What's Fairblock" />
+          <Title title="What's Fairblock?" />
         </div>
         <p className="font-eb-garamond mx-auto p-4 sm:px-8 text-left text-lg lg:text-xl max-w-4xl leading-relaxed break-words">
         Fairblock is a dynamic, decentralized cryptographic computer enabling fast, secure execution of cryptographic logic. It hosts low-cost, high-performance native cApps and seamlessly delivers these capabilities into ecosystems such as Arbitrum, Hyperliquid, Solana, Base, and Cosmos.
