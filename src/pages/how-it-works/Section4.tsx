e2f6d43ef46b535346a6545312c6b5f564ca50ee
pememoni <34263018+pememoni@users.noreply.github.com>
2025-08-25 08:02:51 -0400

    rever-text

--- src/pages/how-it-works/Section4.tsx
+++ src/pages/how-it-works/Section4.tsx
@@ -2,9 +2,9 @@
 import Title from "../../common/Title";
 
 const paragraphs = [
-  "Simplicity is the ultimate form of art. We prioritize simplicity and usability through a multimodal, dynamic design—fine‑tuning performance and security, and introducing complexity only when essential.",
-  "Fairblock leverages DKG and threshold IBE (sometimes referred to as conditional decryption) for applications that require pre‑execution confidentiality—such as sealed‑bid auctions, protected trading, and AI private data marketplaces—achieving minimal bandwidth and computation overhead. For applications requiring computation over encrypted transactions or private data—such as confidential stablecoins, private AI inference, and dark pools—we employ advanced MPC and HE schemes, including Twisted ElGamal, threshold FHE (CKKS), and SPDZ.",
-  "Integrating TEEs with MPC schemes reduces reliance on a single TEE’s hardware security guarantees while lowering the probability of collusion required to compromise an MPC protocol’s honest‑majority assumption. Additionally, ZKPs are used in select MPC applications to ensure the validity of private inputs when multiple parties interact within a shared private state."
+  "Simplicity is the ultimate form of art. We prioritize simplicity and usability through a multimodal, dynamic design fine-tuning performance and security, and introducing complexity only when essential.",
+  "Fairblock leverages DKG and threshold IBE (sometimes referred to as conditional decryption) for applications that require pre-execution confidentiality such as sealed-bid auctions, protected trading, and AI private data marketplaces achieving minimal bandwidth and computation overhead. For applications requiring computation over encrypted transactions or private data such as confidential stablecoins, private AI inference, and dark pools we employ advanced MPC and HE schemes, including Twisted ElGamal, threshold FHE (CKKS), and SPDZ.",
+  "Integrating TEEs with MPC schemes reduces reliance on a single TEE’s hardware security guarantees while lowering the probability of collusion required to compromise an MPC protocol’s honest-majority assumption. Additionally, ZKPs are used in select MPC applications to ensure the validity of private inputs when multiple parties interact within a shared private state."
 ];
 
 const Section4 = () => {
