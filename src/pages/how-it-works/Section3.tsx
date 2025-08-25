995cef425fc579023a5b26f283ee993784d2b3d3
pememoni <34263018+pememoni@users.noreply.github.com>
2025-08-25 08:48:57 -0400

    how-it-works-components

--- src/pages/how-it-works/Section3.tsx
+++ src/pages/how-it-works/Section3.tsx
@@ -1,16 +1,31 @@
 // components
 import Title from "../../common/Title";
-// types 
-import type { HowItWorksCardProps } from "./types"; 
-const HowItWorksCard = ({ imageUrl, title, content }: 
-  HowItWorksCardProps) => { return (
+// types
+import type { HowItWorksCardProps } from "./types";
+
+const HowItWorksCard = ({ imageUrl, title, content }: HowItWorksCardProps) => {
+  return (
+    <div className="bg-primary-blue cursor-pointer flex flex-col gap-8 p-4 lg:py-8 rounded-2xl hover:scale-[1.04] duration-300 transition-all will-change-transform text-black w-full max-w-72 lg:max-w-[310px]">
+      <div className="">
+        <img className="aspect-square mx-auto w-48 lg:w-52" src={imageUrl} alt={title} />
+      </div>
+      <div className="flex flex-col gap-4 text-center">
+        <h3 className="text-3xl">{title}</h3>
+        <p className="text-lg/5 lg:text-xl/5">{content}</p>
+      </div>
+    </div>
+  );
+};
+
+const Section3 = () => {
+  return (
     <div className="flex flex-col items-center p-4 sm:px-8">
-      {/* Main Title */}
       <div className="">
         <Title title={"Confidential Stablecoins Flow"} />
       </div>
-
-      {/* Image */}
+      <div className="mt-12">
+        <Title title="Core Components" />
+      </div>
       <div>
         <img
           className="mx-auto lg:w-[90%]"
@@ -19,17 +34,11 @@ const HowItWorksCard = ({ imageUrl, title, content }:
         />
       </div>
 
-      {/* New Section Title */}
-      <div className="mt-12">
-        <Title title="Core Components" />
-      </div>
-
-      {/* Cards */}
       <div className="flex flex-col sm:flex-row gap-8 lg:gap-16 items-center justify-center mt-8">
         <HowItWorksCard
           imageUrl="/how-it-works/fairy-ring.svg"
           title="Fairyring"
-          content="A dynamic network that hosts app-specific secure and performant confidential computing."
+          content="A dynamic and decentralized network that hosts app-specific secure and performant confidential computing."
         />
         <HowItWorksCard
           imageUrl="/how-it-works/fairy-kit.svg"
@@ -40,3 +49,5 @@ const HowItWorksCard = ({ imageUrl, title, content }:
     </div>
   );
 };
+
+export default Section3;
