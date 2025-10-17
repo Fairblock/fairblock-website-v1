858fadc008d4e7b39f9577e21b3511d1b5f859ae
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-10-17 21:43:17 +0530

    Removed warning

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,7 +5,7 @@ import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
 import Section6 from "./Section6";
-import Section7 from "./Section7";
+// import Section7 from "./Section7";
 
 const Home = () => {
   return (
